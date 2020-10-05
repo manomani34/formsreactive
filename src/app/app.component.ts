ef45a8c947c63c32de5418eb7ee03d9d7adefd8c
manomani34 <59196010+manomani34@users.noreply.github.com>
2020-10-05 14:11:06 +0330

    title
    validations
    dynamic and sync validation
    and ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male' , 'female'];
   inputForm: FormGroup; 
+  banedEmails = ['ali@g.com' , 'a@a.com'];
 
   ngOnInit(): void {
     this.inputForm = new FormGroup({
-      'username' : new FormControl(null, Validators.required),
-      'email' : new FormControl(null,  [Validators.required , Validators.email]),
+      'username' : new FormControl(null, Validators.required , this.bannedusername),
+      'email' : new FormControl(null,  [Validators.required , Validators.email , this.bannedEmailCheck.bind(this)]),
       'gender' : new FormControl('male')
+    });
+
+    //get All of Value of controls in Form
+    // this.inputForm.valueChanges.subscribe(
+    //   (value) =>{
+    //     console.log(value);
+    //   }
+    // );
+
+    //validate All of Form
+    this.inputForm.statusChanges.subscribe(
+      (status) =>{
+        console.log(status);
+      }
+    );
+
+    //fill all of form
+    this.inputForm.setValue({
+      'username' : 'iuiui',
+      'email' : 'g@c.com',
+      'gender' : 'female'
+    });
+
+    //fill all of form
+    this.inputForm.patchValue({
+      'username' : 'iuiui'
     });
   }
 
   onSubmit(){
-    console.log(this.inputForm);
+    console.log(this.inputForm)
+  }
+
+  // custom validator
+  bannedEmailCheck(control: FormControl): {[e: string]: boolean} {
+    if(this.banedEmails.indexOf(control.value) !== -1){
+      return{'isBnnedEmail': true};
+    }
+  }
+
+  bannedusername(control: FormControl): Promise<any> | Observable<any>{
+    const promis = new Promise<any> ((resolve , rejects) => {
+      setTimeout(() => {   
+        if(control.value === 'foad'){
+          resolve({'usernameisbanned' : true});
+        }
+        else{
+          resolve(null);
+        }
+      }, 2000);
+    });
+    return promis;
   }
   
 
